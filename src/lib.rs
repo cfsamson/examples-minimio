407368c127efee51b3a8bc50e4ee6647e1cb7cc0
Carl Fredrik Samson <cf@samson.no>
2019-08-05 01:01:42 +0200

    continued

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,65 @@
+use std::error;
+use std::fmt;
 
-
-#[cfg(target_os="windows")]
+#[cfg(target_os = "windows")]
 pub use windows::{Event, EventLoop, EventResult};
 //#[cfg(target_os="linux")]
 //pub use linux::{Event, EventLoop, EventResult};
 
 const MAXEVENTS: usize = 1000;
 
+#[derive(Debug)]
+pub enum ElErr {
+    IoError(String),
+}
+
+impl fmt::Display for ElErr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use ElErr::*;
+        match self {
+            IoError(s) => write!(f, "IO Error: {}", s),
+        }
+    }
+}
+
+impl error::Error for ElErr {}
+
+impl From<std::io::Error> for ElErr {
+    fn from(e: std::io::Error) -> ElErr {
+        ElErr::IoError(e.to_string())
+    }
+}
 
 mod windows {
+    use crate::ElErr;
+    use std::os::windows::io::{AsRawSocket, RawSocket};
+    use std::sync::atomic::{AtomicUsize, Ordering};
     use std::sync::mpsc::{channel, Receiver, Sender};
     use std::sync::{Arc, Mutex};
-    use std::sync::atomic::{AtomicUsize, Ordering};
     use std::thread;
+
     use super::MAXEVENTS;
 
     pub struct Event<T> {
+        /// Must not be moved, Windows holds raw pointers to this data
         data: T,
+        id: usize,
+        handle: Option<isize>,
+        /// Must not be moved, Windows holds raw pointers to this data
+        wsa_buffers: Vec<ffi::WSABUF>,
+
+        /// Must not be moved, Windows holds raw pointers to this data
+        bytes_recived: u32,
+
     }
-    
-    
+
     impl Event<String> {
         pub fn new_get(url: &str) -> Self {
             Event {
                 data: String::new(),
+                id: 0,
+                handle: None,
+                wsa_buffers: vec![],
             }
         }
 
@@ -51,10 +87,14 @@ mod windows {
     pub struct EventLoop {
         has_error: Arc<AtomicUsize>,
         errors: Arc<Mutex<Vec<String>>>,
+        queue_handle: i32,
     }
 
     impl EventLoop {
-        pub fn new() -> Self {
+        pub fn new() -> Result<Self, ElErr> {
+            // set up the queue
+            let queue_handle = ffi::create_queue()?;
+
             let has_error = Arc::new(AtomicUsize::new(0));
             let errors = Arc::new(Mutex::new(vec![]));
 
@@ -66,7 +106,7 @@ mod windows {
                 let events = vec![ffi::IOCPevent::default(); MAXEVENTS];
                 loop {
                     // TODO: wait for events
-                    
+
                     // handle recieved events
                     let n = 0;
                     let iocp_event = events[n].clone();
@@ -78,18 +118,17 @@ mod windows {
                 }
             });
 
-            EventLoop {
+            Ok(EventLoop {
                 has_error,
                 errors,
-            }
+                queue_handle,
+            })
         }
 
-        pub fn register_soc_read_event<T>(&mut self, soc: RawSocket) {
-            
-        }
+        pub fn register_soc_read_event<T>(&mut self, soc: RawSocket) {}
 
         pub fn poll<T>(&mut self) -> Option<Vec<Event<T>>> {
-            // calling GetQueueCompletionStatus wil either return a handle to a "port" ready to read or 
+            // calling GetQueueCompletionStatus wil either return a handle to a "port" ready to read or
             // block if the queue is empty.
             None
         }
@@ -105,28 +144,37 @@ mod windows {
         }
     }
 
-    pub enum EventResult {
-
-    }
+    pub enum EventResult {}
 
     mod ffi {
+        use crate::ElErr;
+        use std::os::raw::c_void;
+        use std::os::windows::io::RawSocket;
         use std::ptr;
-        use std::os::raw::{c_void, c_int};
-        use std::os::windows::io::{AsRawSocket, RawSocket};
-        #[derive(Debug, Clone)]
-        pub struct IOCPevent {
 
-        }
+        #[derive(Debug, Clone)]
+        pub struct IOCPevent {}
 
         impl Default for IOCPevent {
             fn default() -> Self {
-                IOCPevent {
-
-                }
+                IOCPevent {}
             }
         }
 
-        const INVALID_HANDLE_VALUE: i32 = -1;
+        #[repr(C)]
+        struct WSABUF {
+            len: u32,
+            buf: *mut u8,
+        }
+
+        impl WSABUF {
+            fn new(len: u32, buf: *mut u8) -> Self {
+                WSABUF {
+                    len,
+                    buf,
+                }
+            }
+        }
 
         // Reference: https://docs.microsoft.com/en-us/windows/win32/api/winsock2/ns-winsock2-wsaoverlapped
         #[repr(C)]
@@ -139,15 +187,15 @@ mod windows {
             offset: DWORD,
             /// Reserved for service providers
             offset_high: DWORD,
-            /// If an overlapped I/O operation is issued without an I/O completion routine 
-            /// (the operation's lpCompletionRoutine parameter is set to null), then this parameter 
-            /// should either contain a valid handle to a WSAEVENT object or be null. If the 
-            /// lpCompletionRoutine parameter of the call is non-null then applications are free 
+            /// If an overlapped I/O operation is issued without an I/O completion routine
+            /// (the operation's lpCompletionRoutine parameter is set to null), then this parameter
+            /// should either contain a valid handle to a WSAEVENT object or be null. If the
+            /// lpCompletionRoutine parameter of the call is non-null then applications are free
             /// to use this parameter as necessary.
             h_event: HANDLE,
         }
 
-        https://docs.microsoft.com/en-us/windows/win32/api/minwinbase/ns-minwinbase-overlapped
+        // https://docs.microsoft.com/en-us/windows/win32/api/minwinbase/ns-minwinbase-overlapped
         struct OVERLAPPED {
             internal: ULONG_PTR,
             internal_high: ULONG_PTR,
@@ -156,35 +204,101 @@ mod windows {
         }
 
         // You can find most of these here: https://docs.microsoft.com/en-us/windows/win32/winprog/windows-data-types
-        type HANDLE = *mut c_void;
+        /// The HANDLE type is actually a `*mut c_void` but windows preserves backwards compatibility by allowing
+        /// a INVALID_HANDLE_VALUE which is `-1`. We can't express that in Rust so it's much easier for us to treat
+        /// this as an isize instead;
+        type HANDLE = isize;
         type DWORD = u32;
         type ULONG_PTR = usize;
         type PULONG_PTR = *mut ULONG_PTR;
         type LPDWORD = *mut DWORD;
         type LPWSABUF = *mut u8;
-        type LPWSAOVERLAPPED  = *mut WSAOVERLAPPED;
+        type LPWSAOVERLAPPED = *mut WSAOVERLAPPED;
         type LPOVERLAPPED = *mut OVERLAPPED;
 
-        #[link(name = "win32")]
-        extern "stdcall" {
             // https://referencesource.microsoft.com/#System.Runtime.Remoting/channels/ipc/win32namedpipes.cs,edc09ced20442fea,references
-            //static INVALID_HANDLE_VALUE: c_int;
+            // read this! https://devblogs.microsoft.com/oldnewthing/20040302-00/?p=40443
+            /// Defined in `win32.h` which you can find on your windows system
+            static INVALID_HANDLE_VALUE: HANDLE = -1;
+
+        #[link(name = "Kernel32")]
+        extern "stdcall" {
+            
             // https://docs.microsoft.com/en-us/windows/win32/fileio/createiocompletionport
-            fn CreateIoCompletionPort(filehandle: HANDLE, existing_completionport: HANDLE, completion_key: ULONG_PTR, number_of_concurrent_threads: DWORD) -> HANDLE;
+            fn CreateIoCompletionPort(
+                filehandle: HANDLE,
+                existing_completionport: HANDLE,
+                completion_key: ULONG_PTR,
+                number_of_concurrent_threads: DWORD,
+            ) -> HANDLE;
             // https://docs.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-wsarecv
-            fn WSARecv(s: RawSocket, lpBuffers: LPWSABUF, dwBufferCount: DWORD, lpNumberOfBytesRecvd: LPDWORD, lpFlags: DWORD, lpOverlapped: LPWSAOVERLAPPED) -> i32;
+            fn WSARecv(
+                s: RawSocket,
+                lpBuffers: LPWSABUF,
+                dwBufferCount: DWORD,
+                lpNumberOfBytesRecvd: LPDWORD,
+                lpFlags: DWORD,
+                lpOverlapped: LPWSAOVERLAPPED,
+            ) -> i32;
             // https://docs.microsoft.com/en-us/windows/win32/fileio/postqueuedcompletionstatus
-            fn PostQueuedCompletionStatus(CompletionPort: HANDLE, dwNumberOfBytesTransferred: DWORD, dwCompletionKey: ULONG_PTR, lpOverlapped: LPWSAOVERLAPPED) -> i32;
+            fn PostQueuedCompletionStatus(
+                CompletionPort: HANDLE,
+                dwNumberOfBytesTransferred: DWORD,
+                dwCompletionKey: ULONG_PTR,
+                lpOverlapped: LPWSAOVERLAPPED,
+            ) -> i32;
             // https://docs.microsoft.com/nb-no/windows/win32/api/ioapiset/nf-ioapiset-getqueuedcompletionstatus
-            fn GetQueuedCompletionStatus(CompletionPort: HANDLE, lpNumberOfBytesTransferred: LPDWORD, lpCompletionKey: PULONG_PTR, lpOverlapped: OVERLAPPED, dwMilliseconds: DWORD) -> i32;
+            fn GetQueuedCompletionStatus(
+                CompletionPort: HANDLE,
+                lpNumberOfBytesTransferred: LPDWORD,
+                lpCompletionKey: PULONG_PTR,
+                lpOverlapped: OVERLAPPED,
+                dwMilliseconds: DWORD,
+            ) -> i32;
             // https://docs.microsoft.com/nb-no/windows/win32/api/handleapi/nf-handleapi-closehandle
             fn CloseHandle(hObject: HANDLE) -> i32;
         }
+
+        // ===== SAFE WRAPPERS =====
+
+        pub fn create_queue() -> Result<i32, ElErr> {
+            unsafe {
+                // number_of_concurrent_threads = 0 means use the number of physical threads but the argument is
+                // ignored when existing_completionport is set to null.
+                let res = CreateIoCompletionPort(INVALID_HANDLE_VALUE, 0, 0, 0);
+                if (res as *mut usize).is_null() {
+                    return Err(std::io::Error::last_os_error().into());
+                }
+                Ok(*(res as *const i32))
+            }
+        }
+
+        pub fn create_soc_read_event(s: RawSocket, wsabuffers: &mut [WSABUF], bytes_recieved: &mut u32) -> Result<(), ElErr> {
+            let buf: *mut u8 = buffer.as_mut_ptr();
+            // This actually takes an array of buffers but we will only need one so we can just box it
+            // and point to it (there is no difference in memory between a `vec![T; 1]` and a `Box::new(T)`)
+            let buff_ptr: *mut WSABUF = wsabuffers.as_mut_ptr();
+            let num_bytes_recived_ptr: *mut u32 = bytes_recieved;
+            let lp_flags = 
+
+            unsafe {
+                let res = WSARecv(s, )
+            }
+        }
+
+        pub fn register_event(completion_port: )
+
+        #[cfg(test)]
+        mod tests {
+            use super::*;
+            #[test]
+            fn create_queue_works() {
+                let queue = create_queue().unwrap();
+                assert!(queue > 0);
+            }
+        }
     }
 }
-
-
-
 
 #[cfg(test)]
 mod tests {
--- tests/basic.rs
+++ tests/basic.rs
@@ -1,17 +1,17 @@
 use minimio::{EventLoop, Event};
 
-#[test]
-fn main() {
+// #[test]
+// fn main() {
 
-    let mut event_loop = EventLoop::new();
+//     let mut event_loop = EventLoop::new();
 
-    let id = event_loop.register_event(Event::new_get("www.google.com"));
+//     let id = event_loop.register_event(Event::new_get("www.google.com"));
 
-    while let Ok((event_id, data)) = event_loop.wait() {
-        if let id == event_id {
-            println!("got a response: ", data);
-        }
-    }
-}
+//     while let Ok((event_id, data)) = event_loop.wait() {
+//         if let id == event_id {
+//             println!("got a response: ", data);
+//         }
+//     }
+// }
 
 
